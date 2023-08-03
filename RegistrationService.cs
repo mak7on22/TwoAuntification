647b58879be5a8fc57a07ab68a00966f7498106b
mak7on22 <mur-mur-0998@mail.ru>
2023-08-04 04:00:43 +0600

    added class RegistrationServise

---
+++ RegistrationService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBitcoin;
+
+namespace TwoAuntification
+{
+    public class RegistrationService
+    {
+        private List<User> users;
+
+        public List<User> Users
+        {
+            get { return users; }
+        }
+
+        public RegistrationService()
+        {
+            users = new List<User>();
+        }
+
+        public void RegisterUser()
+        {
+            Console.WriteLine("Введите ваше Имя и Фамилию:");
+            string fullName = Console.ReadLine()!;
+
+            // Генерация публичного и приватного ключей для нового пользователя
+            Key privateKey = new();
+            PubKey publicKey = privateKey.PubKey;
+
+            var newUser = new User
+            {
+                FullName = fullName,
+                PublicKey = publicKey,
+                PrivateKey = privateKey
+            };
+
+            users.Add(newUser);
+
+            Console.WriteLine($"Пользователь {fullName} успешно зарегистрирован.");
+        }
+
+        public bool AuthenticateUser(Key privateKey)
+        {
+            Console.WriteLine("Введите ваше Имя и Фамилию:");
+            string fullName = Console.ReadLine()!;
+
+            var user = users.Find(u => u.FullName == fullName);
+
+            if (user == null)
+            {
+                Console.WriteLine("Пользователь не найден.");
+                return false;
+            }
+
+            if (user.PrivateKey == privateKey)
+            {
+                Console.WriteLine($"Выполнен вход под {fullName}");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Вход не выполнен. Неверный приватный ключ.");
+                return false;
+            }
+        }
+    }
+}
