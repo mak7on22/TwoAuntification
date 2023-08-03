05688c54e1d8f10eb5612b568c4748a47a25261f
mak7on22 <mur-mur-0998@mail.ru>
2023-08-04 03:57:36 +0600

    added class Users and features

---
+++ User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBitcoin;
+
+namespace TwoAuntification
+{
+
+    public class User
+    {
+        public string? FullName { get; set; }
+        public PubKey? PublicKey { get; set; }
+        public Key? PrivateKey { get; set; }
+    }
+}
