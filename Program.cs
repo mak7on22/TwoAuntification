80bec56d67cfa8ab54b55551aad732f94bf71c4a
mak7on22 <mur-mur-0998@mail.ru>
2023-08-04 03:59:41 +0600

    added class Program and register two users

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using NBitcoin;
+namespace TwoAuntification
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var registrationService = new RegistrationService();
+
+            // Регистрация первого пользователя
+            registrationService.RegisterUser();
+
+            // Регистрация второго пользователя
+            registrationService.RegisterUser();
+
+            // Попытка аутентификации первого пользователя
+            bool isUser1Authenticated = registrationService.AuthenticateUser(registrationService.Users[0].PrivateKey!);
+
+            // Попытка аутентификации второго пользователя
+            bool isUser2Authenticated = registrationService.AuthenticateUser(registrationService.Users[1].PrivateKey!);
+        }
+    }
+}
